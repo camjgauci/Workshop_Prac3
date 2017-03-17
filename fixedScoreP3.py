b7525f3d34afb2af14ed441915870d92b8c15911
jc350949 <cameron.gauci@my.jcu.edu.au>
2017-03-17 14:57:36 +1000

    Add fixedScoreP3 Function

--- fixedScoreP3.py
+++ fixedScoreP3.py
@@ -1,16 +1,25 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
-def main():
 
-# TODO: Fix this!
-score = float(input("Enter score: "))
-if  0 > score > 100:
-    print("Invalid score")
-elif 50 > score > 90:
-        print("Passable")
-elif score > 90:
-    print("Excellent")
-else:
-      print("Bad")
+# score = float(input("Enter score: "))
+# if  0 > score > 100:
+#     print("Invalid score")
+# elif 50 > score > 90:
+#     print("Passable")
+# elif score > 90:
+#     print("Excellent")
+# else:
+#       print("Bad")
+
+
+def score_result():
+    score = float(input("Enter Score:"))
+    if 0 < score > 100:
+        return "Invalid Score"
+    elif 50 < score < 90:
+        return "Passable"
+    elif score > 90:
+        return "Excellent"
+    else:
+        return "Bad"
+
+
+print(score_result())
